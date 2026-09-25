1f4e37c132da93965cb5c704e924121cb8fa220f
MysticYew <105715246+MysticYewXd@users.noreply.github.com>
2026-09-25 20:48:44 +0530

    Update system_volume_provider.dart

--- lib/core/audio/system_volume_provider.dart
+++ lib/core/audio/system_volume_provider.dart
@@ -2,14 +2,14 @@ import 'dart:io';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'android_system_volume_controller.dart';
 import 'linux_system_volume_controller.dart';
+import 'plugin_system_volume_controller.dart';
 import 'system_volume_controller.dart';
 
 final systemVolumeControllerProvider = Provider<SystemVolumeController>((ref) {
   final controller = Platform.isLinux
       ? LinuxSystemVolumeController()
-      : AndroidSystemVolumeController();
+      : PluginSystemVolumeController();
   ref.onDispose(controller.dispose);
   return controller;
 });
